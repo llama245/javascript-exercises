b50effe28a53a9504145dbb74bf43f31cd461236
llama245 <lorenbap77@outlook.com>
2023-02-11 01:05:11 -0500

    passed tests

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string,times) {
+    let text = "";
+
+    if( times < 0 ) {
+        return "ERROR"
+    }
+
+    for (let i = 0; i < times ; i ++) {
+        if (times >= 0) {
+        text += string;
+        }
+    } 
+
+    return text
+
+    
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
