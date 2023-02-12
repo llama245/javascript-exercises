bc66c8334e290df33b03b6b3cf3f5aa97ffb4ca9
llama245 <lorenbap77@outlook.com>
2023-02-11 23:58:35 -0500

    passed test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,44 @@
-const sumAll = function() {
+const sumAll = function(x,y) {
+
+    if (x < 0 ) {
+        return "ERROR"
+    }
+
+    else if (y < 0) {
+        return "ERROR"
+    }
+
+    if (typeof x != 'number') {
+        return "ERROR"
+    }
+
+    else if (typeof y != 'number') {
+        return "ERROR"
+    }
+
+    let array = [ ]
+    
+    let value_x = x
+    let value_y = y 
+    sum = 0 
+
+    if (x < y) {
+        for (let i = value_x; i <= value_y; i ++) {
+            sum += i;
+            //array.push(i);
+
+    }
+    }
+
+    else if (x > y) {
+        for (let i = value_y; i <= value_x; i++) {
+            sum += i
+    }
+    }
+    
+
+    return sum        
+    
 
 };
 
