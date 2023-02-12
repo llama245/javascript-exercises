4fe50a3c000d7c929146da2865522dcf45e8094f
llama245 <lorenbap77@outlook.com>
2023-02-12 00:37:29 -0500

    passed tests, previously completed excercise in Python

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature_f) {
+  celsius_temp = (temperature_f - 32) / 1.8
+
+  celsius_rounded = Math.round(celsius_temp * 10) / 10
+
+  return celsius_rounded
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature_c) {
+    f_temp = (temperature_c * 1.8) + 32
+    f_temp_rounded = Math.round(f_temp * 10) /10
+    return f_temp_rounded
 };
+
 
 // Do not edit below this line
 module.exports = {
