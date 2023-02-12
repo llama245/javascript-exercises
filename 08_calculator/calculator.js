3c2152a60138670ccdf8e0d5d587ad23104b1ad9
llama245 <lorenbap77@outlook.com>
2023-02-12 18:29:46 -0500

    completed test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
+const add = function(a, b) {
+  return a + b; 
+};
+
+const subtract = function(a, b) {
+  return a - b;
 	
 };
 
-const subtract = function() {
+const sum = function(nums) {
+  const sum1 = nums.reduce((accumulator, current) => {
+    return accumulator + current;
+ }, 0);
+ return sum1;
 	
 };
 
-const sum = function() {
+const multiply = function(nums) {
+
+  return nums.length 
+    ? nums.reduce((accumulator, nextItem) =>
+    accumulator * nextItem)
+    : 1;
+
+};
+
+const power = function(a,b) {
+  return Math.pow(a, b)
 	
 };
 
-const multiply = function() {
+const factorial = function(number) {
+  let result = number;
 
+  if (number === 0 || number === 1) {
+    return 1
+  }
+  
+  while (number > 1){
+    number --;
+    result *= number;
+  }
+  return result;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
