152d494cb257d9163dd3d66b77791a64803bcd43
llama245 <lorenbap77@outlook.com>
2023-02-12 19:42:11 -0500

    passed all tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const palindrome = string.toLowerCase().replace(/[^a-z]/g, "");
+    return (palindrome 
+        .split("")
+        .reverse("")
+        .join("") == palindrome);
 };
 
 // Do not edit below this line
