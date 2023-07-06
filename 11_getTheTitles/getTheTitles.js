2567a4558f25aaa7e81accc2f8c514b6a0c51e9d
llama245 <lorenbap77@outlook.com>
2023-07-06 06:06:06 -0400

    changes to htm; performing first test

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,10 +1,27 @@
-const getTheTitles = function() {
 
-   Object.keys(title).map(function(title){
-    return title[key];
-   })
+
+
+const getTheTitles = function() {
    
+   const books = [
+      {
+         title: 'Book',
+         author: 'Name'
+      },
+      {
+         title: 'Book2',
+         author: 'Name2'
+      }
+   ]
+
+   const titles = []
+   books.forEach(element => {titles.push(title)
+      
+   });  
+   return titles 
 };
 
+console.log(getTheTitles)
+
 // Do not edit below this line
-module.exports = getTheTitles;
+//module.exports = getTheTitles;
