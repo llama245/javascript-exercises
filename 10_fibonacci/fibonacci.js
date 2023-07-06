1ebe4de5c7f61714d371df9f81bee31051094397
llama245 <lorenbap77@outlook.com>
2023-07-06 04:35:28 -0400

    trying to solve "not function" error

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -2,24 +2,15 @@ const fibonacci = function(number) {
     Number(number) 
     let i = 2;
     let limit = 100000;
-
-    const fib = [0,1]
- 
+    const fib = [0,1];
     
-    for( i; i < limit; i++){
+    for(i; i < limit; i++){
         fib[i] = fib[i-2] + fib[i-1];
-        if(fib[i] < 0){
-            fib.push(1)
-        }
-        else{
-            fib.push(fib[i])
-        }
-        
+        fib.push(fib[i]);
     };
     
-    return fib[number]
+    console.log(fib[number]);
 };
 
 
-
-console.log(fibonacci(8))
+fibonacci("6")
